331c70fc61c01fe85bca9dca3d6a6ce4aca59c3c
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-01-18 19:45:39 +0900

    Time: 1549 ms (47.54%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,11 @@
+select name
+from salesperson
+where sales_id not in (
+    select sales_id
+    from orders
+    where com_id in (
+        select com_id 
+        from company
+        where name = 'red'
+    )
+)
