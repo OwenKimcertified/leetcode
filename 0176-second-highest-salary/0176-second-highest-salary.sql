096e73cc474e5d04679d9b6927f9fc734cb8512a
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-01-23 21:21:22 +0900

    Time: 392 ms (32.42%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,13 @@
-select max(salary) as secondhighestsalary
+select
+    max(salary) as secondhighestsalary
 from employee
-where salary not in (select max(salary) 
-                     from employee);
+where salary < (
+    select max(salary)
+    from employee
+) 
+
+# max는 값이 없으면 null 을 리턴함.
+# 두 번째로 큰 salary 를 찾기 때문에 조건에 맞지 않다 -> 
+# 두 번째로 큰 값이 없다 ->
+# 1개 혹은 0 개 -> 
+# 문제에서 null 로 출력을 요청 !
