7eff70823e77d7f535ed11bd9da8364400adfc85
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-02-21 21:01:58 +0900

    Time: 815 ms (47.13%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,3 +2,6 @@ select activity_date as day, count(distinct user_id) as active_users
 from activity
 where (activity_date > "2019-06-27" and activity_date <= "2019-07-27")
 group by activity_date;
+
+# why it doesnt work? 2019-06-27 < activity_date <= "2019-07-27"
+# this code includes date 2019-06-25......
