65309a8764683afb9252a4f188fba992a110c5fa
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-02-23 21:50:17 +0900

    Time: 844 ms (16.35%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+create function getNthHighestSalary(N int) returns int
+begin
+declare x int;
+set x = N-1;
+  RETURN (
+      select salary
+      from employee
+      group by salary
+      order by salary desc
+      limit x, 1
+  );
+END
