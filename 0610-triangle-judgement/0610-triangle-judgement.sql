ac54fbec70d6c22f5506fe3157decb782506b212
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-08-09 22:58:04 +0900

    Time: 489 ms (86.83%), Space: 0B (100.00%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select x, y, z, case 
+                    when x + y > z and x + z > y and y + z > x then 'Yes'
+                        else 'No'
+                        end as 'Triangle'
+                        
+from triangle;
