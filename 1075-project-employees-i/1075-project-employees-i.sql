885d535f0e9f43881c70eb02ba39694da74b365e
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-08-09 23:25:17 +0900

    Time: 1685 ms (50.92%), Space: 0B (100.00%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.project_id, cast(avg(e.experience_years) as decimal(5, 2)) as average_years
+from Project as p
+left join Employee as e on p.employee_id = e.employee_id
+GROUP BY p.project_id
+order by p.project_id asc;
