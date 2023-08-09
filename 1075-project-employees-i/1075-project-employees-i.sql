e055d0b823d2b906f8c3548b06f077701953fa7b
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-08-09 23:25:48 +0900

    Time: 1419 ms (88.55%), Space: 0B (100.00%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -2,5 +2,5 @@
 select p.project_id, cast(avg(e.experience_years) as decimal(5, 2)) as average_years
 from Project as p
 left join Employee as e on p.employee_id = e.employee_id
-GROUP BY p.project_id
+group by p.project_id
 order by p.project_id asc;
