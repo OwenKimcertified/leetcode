f03a5b5a44b9bba34d10bc9c2c8a63bcb78f2a59
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-09-01 22:46:04 +0900

    Time: 2307 ms (8.06%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -5,6 +5,6 @@ left join orders as o on p.product_id = o.product_id
 where o.order_date between '2020-02-01' and '2020-02-29'
 group by p.product_id
 having unit >= 100
-
+order by o.product_id;
 
 
