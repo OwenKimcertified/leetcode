151e802faaf0e5ca0f73d8b57630416a95448854
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-10-09 05:21:48 +0900

    Time: 45 ms (23.64%), Space: 16.3 MB (29.99%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,15 +1,15 @@
 class Solution:
     def generate(self, numRows):
-        double_list = [[1]]
+        self.start = [[1]]
         
         for i in range(1, numRows):
-            current_row = [1]
-            prev_row = double_list[-1]
+            self.curr_row, self.prev_row = [1], self.start[-1]
+            
             
             for j in range(1, i):
-                current_row.append(prev_row[j - 1] + prev_row[j])
+                self.curr_row.append(self.prev_row[j - 1] + self.prev_row[j])
             
-            current_row.append(1)
-            double_list.append(current_row)
+            self.curr_row.append(1)
+            self.start.append(self.curr_row)
         
-        return double_list
+        return self.start
