0907e2f61cbbf9777de92e256484752d340c2da9
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-10-19 22:23:11 +0900

    Time: 558 ms (98.80%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,15 +1,7 @@
-# select  query_name, 
-#         round(avg(rating/position), 2) as quality
-# ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(rating)*100), 2) AS poor_query_percentage
-# from Queries
-# group by query_name
-
-# round((sum(case when rating < 3 then 1 else 0 end) /count(query_name))*100,2) as poor_query_percentage 
-
-select
-    query_name,
-    round(sum(rating*1.0/position)/count(query_name) , 2) as quality,
-    round((sum(case when rating < 3 then 1.0 else 0 end)/count(query_name)) * 100 , 2)
-        as poor_query_percentage
+select  query_name, 
+        round(avg(rating/position), 2) as quality,
+        round((sum(case when rating < 3 then 1 else 0 end)/ count(rating)*100), 2)
+               as poor_query_percentage
 from Queries
 group by query_name
+
