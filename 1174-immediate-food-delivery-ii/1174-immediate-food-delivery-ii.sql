359da0a0795f61c84f4f2566edcd45184eb8f1d4
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2023-11-04 20:56:37 +0900

    Time: 1318 ms (31.90%), Space: 0B (100.00%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,12 @@
+# first order -> min() func
+# 1 immediate, 0 scheduled -> case
+
+select round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/
+             count(*)*100, 2) as immediate_percentage
+from delivery
+where (customer_id, order_date) in (select customer_id, min(order_date)
+                                    from delivery
+                                    group by customer_id)
+
+
+
