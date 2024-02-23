b4c872f7cdf3fc3833c5e08614791af49e16ec49
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-02-24 03:37:58 +0900

    Time: 1775 ms (10.91%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,17 +1,12 @@
 # Write your MySQL query statement below
 # caution! else 0 시 카운트 됨을 유념. 더하기의 개념이 아님. 
 
-# select 
-# s.user_id,
-# round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(*), 2) as confirmation_rate 
+select 
+s.user_id,
+round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(*), 2) as confirmation_rate 
 
-# from confirmations as c
-# right join signups as s on c.user_id = s.user_id 
+from confirmations as c
+right join signups as s on c.user_id = s.user_id 
 
-# group by c.user_id
-
-select s.user_id,
-round(sum(case when action='confirmed' then 1 else 0 end)/count(*) ,2) as confirmation_rate
-from signups s
-left join Confirmations c on s.user_id = c.user_id
 group by s.user_id
+
