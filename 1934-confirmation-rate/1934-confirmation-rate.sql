fd7f72209f34ce92367b756497d97062998212bc
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-02-24 03:38:20 +0900

    Time: 1228 ms (56.55%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -3,7 +3,7 @@
 
 select 
 s.user_id,
-round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(*), 2) as confirmation_rate 
+round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(s.user_id), 2) as confirmation_rate 
 
 from confirmations as c
 right join signups as s on c.user_id = s.user_id 
