8c4ba87e85b412da5f6f453e9be17090191684e6
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-03-13 19:24:18 +0900

    Time: 987 ms (9.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+select
+id,
+visit_date,
+people
+
+from (select *,
+             rank() over(order by id asc) as icmt,
+             (id - rank() over(order by id asc)) as checker
+      from stadium
+      where people >= 100 
+      ) as sub_table
+
+where 
+checker in (select is_consecutive
+            from (select *,
+                  rank() over(order by id asc) as icmt,
+                  (id - rank() over(order by id asc)) as is_consecutive
+                  from stadium
+                  where people >= 100) as sub_table2
+            group by is_consecutive
+            having(count(*)) >= 3)
+
+order by visit_date asc; 
