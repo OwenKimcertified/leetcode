f4122ebe03e2df1e0ab3d67bbfc42ec863da0f2b
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-03-20 00:42:47 +0900

    Time: 21 ms (96.21%), Space: 12.5 MB (7.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,10 +2,13 @@ class Solution(object):
     def searchInsert(self, nums, target):
         if target <= nums[0]:
             return 0
+        
         elif target > nums[-1]:
             return len(nums)
+        
         left = 0
         right = len(nums) - 1
+        
         while left != right - 1:
             mid = (left + right) // 2
             if target > nums[mid]:
@@ -14,4 +17,5 @@ class Solution(object):
                 right = mid
             else:
                 return mid
+        
         return right
