1e65463eda232b91044ea872ae53bcbc9f611cf6
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-03-19 22:55:29 +0900

    Time: 39 ms (28.21%), Space: 18.6 MB (90.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -15,11 +15,10 @@ class Solution(object):
         
         slow, fast = head, head 
         
-        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next 
-            if(slow == fast):
+            if slow == fast:
                 return True
         
         return False        
